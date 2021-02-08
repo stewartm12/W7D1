011aabd1328651546eec7e46760f51b0c1d3e5b3
Ariton Sefedini <aritonsefedini@icloud.com>
2021-02-08 14:26:35 -0500

    Added controllers

--- skeleton/app/controllers/application_controller.rb
+++ skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+    def login!(user)
+        user.reset_session_token!
+        self.session[:session_token] = user.session_token
+        user.session_token
+    end
+    
 end
--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       post :deny
     end
   end
+  resources :users
+  resource :session, only: [:new, :create, :destroy]
 
   root to: redirect('/cats')
 end
